9306e7b1079d6e684e6159fbc6623146943555b0
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-01-04 20:26:48 +0500

    Delete printEntry.jsx

--- printEntry.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import BarcodePrint from "./pages/BarcodePrint";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BarcodePrint />
-  </React.StrictMode>
-);
